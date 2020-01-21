b71a840f640c8e5702f675e333f7dadb6b7480c0
Juliette Pagacz <jpagacz@w-r10-p2.s19.be>
2020-01-21 19:19:22 +0100

    almost there

--- get_next_line.c
+++ get_next_line.c
@@ -1,34 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line4.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jpagacz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/25 13:19:44 by jpagacz           #+#    #+#             */
-/*   Updated: 2020/01/14 14:35:25 by jpagacz          ###   ########.fr       */
+/*   Created: 2020/01/14 14:35:37 by jpagacz           #+#    #+#             */
+/*   Updated: 2020/01/21 19:17:09 by jpagacz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	get_next_line(int fd, char **line)
+int    get_next_line(int fd, char **line)
 {
-	int			ret;
-	int			i;
-	char		buf[BUFFER_SIZE + 1];
-	static char	*reste;
-	char		*tmp;
-	char		*tmp2;
+	int				ret;
+	int				i;
+	char			buf[BUFFER_SIZE + 1];
+	static char		*reste;
+	char			*tmp;
+	char			*tmp2;
 
 	i = 0;
-	tmp = 0;
-	tmp2 = 0;
-	if (BUFFER_SIZE < 1 || fd < 0 || !line)
+	tmp = NULL;
+	tmp2 = NULL;
+	if (!reste) {
+		reste = malloc(sizeof(char) * 1);
+		reste[0] = 0;
+	}
+	if (BUFFER_SIZE < 1 || !line || (ret = read(fd, buf, 0) < 0))
 		return (-1);
-	while (ft_strchr(buf, '\n') == -1 && ret)
-	{	
-		ret = read(fd, buf, BUFFER_SIZE);	
+	while ((ret = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
 		buf[ret] = '\0';
 		if (reste == NULL)
 			reste = ft_strdup(buf);
@@ -37,26 +40,34 @@ int	get_next_line(int fd, char **line)
 			tmp = ft_strjoin(reste, buf);
 			free(reste);
 			reste = tmp;
-		}		
+		}
+		if (ft_strchr(reste, '\n'))
+			break ;
 	}
-	if (reste[i])
-	{		
+	if (ft_strchr(reste, '\n'))
+	{
 		while (reste[i] != '\n' && reste[i])
 			i++;
 		*line = ft_substr(reste, 0, i);
 		tmp2 = ft_strdup(&reste[i + 1]);
-		if (ft_strchr(reste, '\n') == 1)
-		{
-			free(reste);
-			reste = tmp2;	
-			return (1);
-		}
-		else
-		{
-			free(reste);
-			reste = tmp2;	
-			return (0);
-		}
+		free(reste);			
+		reste = tmp2;
+		return (1);
 	}
-	return (0);
+	else if (reste[i] == '\n')
+	{	
+		*line = ft_substr(reste, 0, i);
+		tmp2 = ft_strdup(&reste[i + 1]);
+		free(reste);			
+		reste = tmp2;
+		return (1);
+	}
+	else
+	{
+		*line = ft_strdup(reste);
+		free(reste);
+		reste = tmp2;
+		return (0);
+	}
+	return (1);
 }
