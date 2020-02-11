b50f735634ae3974452df8d619024274abcba9cd
Juliette Pagacz <jpagacz@e-r11-p3.s19.be>
2020-02-11 09:09:22 +0100

    working

---
+++ get_next_line.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpagacz <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/24 10:28:43 by jpagacz           #+#    #+#             */
+/*   Updated: 2020/02/10 18:08:02 by jpagacz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static void	ft_del(char **str)
+{
+	free(*str);
+	*str = NULL;
+}
+
+int			ft_printline(char **stock, char **line, int fd)
+{
+	int		size;
+	char	*tmp;
+
+	tmp = NULL;
+	if ((size = ft_strchr(stock[fd], '\n')) > -1)
+	{
+		if (!(*line = ft_substr(stock[fd], 0, size)))
+			return (-1);
+		if (!(tmp = ft_strdup(&stock[fd][size + 1])))
+			return (-1);
+		ft_del(&stock[fd]);
+		stock[fd] = tmp;
+		return (1);
+	}
+	if (!(*line = ft_strdup(stock[fd])))
+		return (-1);
+	ft_del(&stock[fd]);
+	return (0);
+}
+
+int			get_next_line(int fd, char **line)
+{
+	int			size;
+	char		buf[BUFFER_SIZE + 1];
+	char		*tmp;
+	static char	*stock[FOPEN_MAX];
+
+	tmp = NULL;
+	if (fd < 0 || fd > OPEN_MAX || BUFFER_SIZE <= 0
+			|| !line || (size = read(fd, buf, 0) < 0)
+			|| (!stock[fd] && !(stock[fd] = ft_strdup(""))))
+		return (-1);
+	while ((ft_strchr(stock[fd], '\n') == -1)
+			&& (size = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+		buf[size] = '\0';
+		if (!stock[fd] && !(stock[fd] = ft_strdup(buf)))
+			return (-1);
+		else if (!(tmp = ft_strjoin(stock[fd], buf)))
+			return (-1);
+		ft_del(&stock[fd]);
+		stock[fd] = tmp;
+	}
+	return (ft_printline(stock, line, fd));
+}
---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpagacz <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/24 10:36:33 by jpagacz           #+#    #+#             */
+/*   Updated: 2020/02/10 17:27:55 by jpagacz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
+int			get_next_line(int fd, char **line);
+size_t		ft_strlen(char *s);
+char		*ft_strdup(char *s1);
+char		*ft_substr(char *s, unsigned int start, size_t len);
+char		*ft_strjoin(char *s1, char *s2);
+int			ft_strchr(char *s, int c);
+
+#endif
